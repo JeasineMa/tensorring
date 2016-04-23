a0ef46a1fc9fd243c024c864de63ed31b12fda2c
JeasineMa <ma_tony@163.com>
2016-04-23 10:43:37 +0800

    [Add]some changes

---
+++ data_io/build/CMakeFiles/jy901_serial.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/include/moc_ui.cxx" "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/CMakeFiles/jy901_serial.dir/include/moc_ui.cxx.o"
+  "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/jy901_serial_automoc.cpp" "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/CMakeFiles/jy901_serial.dir/jy901_serial_automoc.cpp.o"
+  "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/src/Serial.cpp" "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/CMakeFiles/jy901_serial.dir/src/Serial.cpp.o"
+  "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/src/main.cpp" "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/CMakeFiles/jy901_serial.dir/src/main.cpp.o"
+  "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/src/ui.cpp" "/Users/jeasinema/Documents/Skyworks/GitHub/tenserflow_ring/data_io/build/CMakeFiles/jy901_serial.dir/src/ui.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/local/Cellar/qt/4.8.7_2/include"
+  "/usr/local/Cellar/qt/4.8.7_2/lib/QtCore.framework"
+  "/usr/local/Cellar/qt/4.8.7_2/include/QtGui"
+  "/usr/local/Cellar/qt/4.8.7_2/lib/QtCore.framework/Headers"
+  "/usr/local/Cellar/qt/4.8.7_2/mkspecs/default"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ data_io/build/CMakeFiles/jy901_serial_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "jy901_serial_automoc.cpp"
+  "CMakeFiles/jy901_serial_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/jy901_serial_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
